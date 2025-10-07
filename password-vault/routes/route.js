71bf2e40c9839ee8f079ff11285758b73f725929
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-08 01:59:30 +0600

    Formatted the code

--- password-vault/routes/route.js
+++ password-vault/routes/route.js
@@ -1,9 +1,15 @@
 import { parseReqBody } from "../Common/common.js";
 
 const routes = [
-    { method: 'GET', path: '/', handler: async () => ({ message: "Welcome" }) },
     {
-        method: 'GET', path: '/password/names', handler: async (req, res) => {
+        method: 'GET',
+        path: '/',
+        handler: async () => ({ message: "Welcome" })
+    },
+    {
+        method: 'GET',
+        path: '/password/names',
+        handler: async (req, res) => {
             const data = await parseReqBody(req);
 
             res.writeHead(200, { "Content-Type": "application/json" });
