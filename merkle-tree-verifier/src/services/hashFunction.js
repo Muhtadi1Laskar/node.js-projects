45736886efc7931dedf7438976d1e6d1890724ae
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-12 22:14:46 +0600

    Added build function in Merkle Tree

--- merkle-tree-verifier/src/services/hashFunction.js
+++ merkle-tree-verifier/src/services/hashFunction.js
@@ -1,7 +1,7 @@
 import crypto from "node:crypto";
 
 const hashData = (data) => {
-    return crypto.create("sha256").update(data).digest("hex");
+    return crypto.createHash("sha256").update(data).digest("hex");
 }
 
 export {
--- merkle-tree-verifier/src/services/merkleTree.js
+++ merkle-tree-verifier/src/services/merkleTree.js
@@ -7,5 +7,41 @@ class MerkleTree {
         this.root = this.levels[this.levels.length - 1][0];
     }
 
-    
+    build(leaves) {
+        if (leaves.length === 0) return [[]];
+
+        const levels = [leaves];
+        let currentLevel = leaves;
+
+        while (currentLevel.length > 1) {
+            const nextLevel = [];
+            const length = currentLevel.length;
+
+            for (let i = 0; i < length; i += 2) {
+                const left = currentLevel[i];
+                const right = currentLevel[i + 1] || currentLevel[i];
+                nextLevel.push(hashData(left + right));
+
+            }
+            levels.push(nextLevel);
+            currentLevel = nextLevel;
+        }
+        return levels;
+    }
+
+    getRoot() {
+        return this.root;
+    }
 }
+
+const documentChunks = [
+    "Blockchain ensures data integrity through consensus.",
+    "Merkle trees allow efficient verification of data chunks.",
+    "Each leaf node represents a hashed data block.",
+    "Parent nodes store hashes of their children.",
+    "The root hash summarizes the entire dataset."
+];
+
+const tree = new MerkleTree(documentChunks);
+
+console.log(tree.getRoot());
