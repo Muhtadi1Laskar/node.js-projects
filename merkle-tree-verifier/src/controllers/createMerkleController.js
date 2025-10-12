af52238f6a8a11fd75dc053465be2c606f92bf69
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-13 00:04:41 +0600

    Added the controller for creatring Merkle root

--- merkle-tree-verifier/src/controllers/createMerkleController.js
+++ merkle-tree-verifier/src/controllers/createMerkleController.js
@@ -1,7 +1,22 @@
+import MerkleTree from "../services/merkleTree.js";
 import { writeResponse } from "../utils/utils.js";
 
 async function createMerkleController(res, reqBody) {
-    writeResponse(res, { message: "Hello Bitch" });
+    const { data } = reqBody;
+
+    if(data.length < 0) {
+        writeResponse(res, {
+            message: "No documents provided"
+        });
+        return;
+    }
+
+    const tree = new MerkleTree(data);
+    const root = tree.getRoot();
+
+    writeResponse(res, {
+        root: root
+    });
     return;
 }
 
