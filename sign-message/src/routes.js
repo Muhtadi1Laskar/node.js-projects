327085005d29b8c415cf41f8b93e872c692c705c
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-13 13:45:49 +0600

    Added the routes files

---
+++ sign-message/src/routes.js
@@ -0,0 +1,16 @@
+const routes = {
+    "POST:/generateKey": {
+        controller: '',
+        schema: '',
+    },
+    "POST:/sign-message": {
+        controller: '',
+        schema: '',
+    },
+    "POST:/verify-messsage": {
+        controller: '',
+        schema: '',
+    }
+};
+
+export default routes;
