b9cc69961bde66175d4210a56340221973f59ce6
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-17 19:52:08 +0600

    Added the register controller

---
+++ note-taker/src/controllers/user.controller.js
@@ -0,0 +1,24 @@
+import { errorResponse, successResponse } from "../utils/response";
+
+const registerUser = async (res, body) => {
+    const { name, email, password } = body;
+
+    if(!email.test(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+        errorResponse(res, { message: 'Invalid email format' }, 403);
+        return;
+    }
+
+    if(password.length <= 8) {
+        errorResponse(res, { message: 'Password should be longer than 8 characters' }, 403);
+        return;
+    }
+
+    try {
+        const user = await createUser(body);
+        successResponse(res, user, 201);
+    } catch (error) {
+        errorResponse(res, { message: error }, 403);
+    }
+}
+
+export default registerUser;
