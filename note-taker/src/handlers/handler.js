6892bc58518a4d044013dddb562352e034439fd7
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-17 19:33:16 +0600

    Added the handler function

--- note-taker/src/handlers/handler.js
+++ note-taker/src/handlers/handler.js
@@ -1,15 +1,38 @@
 import routes from "../routers/routes.js";
 import { errorResponse, successResponse } from "../utils/response.js";
 
-async function handler(req, res) {
+export default async function handler(req, res) {
     const { method, url } = req;
     const endpoint = `${method}:${url}`;
     const route = routes[endpoint];
 
-    if(!route.controller) {
-        errorResponse(404, {
+    if (!route.controller) {
+        errorResponse(res, {
             message: "Invalid endpoint"
-        });
+        }, 404);
         return;
+    }
+
+    let body = "";
+    if (method === "POST" || method === "PUT" || method === "DELETE") {
+        const rawReqBody = await parseRequestBody(req);
+        const { valid, message } = validateSchema(req, route.schema);
+
+        if (!valid) {
+            errorResponse(res, {
+                message
+            }, 403);
+            return
+        }
+
+        body = rawReqBody;
+    }
+
+    try {
+        await route.controller(res, body);
+    } catch (error) {
+        errorResponse(res, {
+            message: error
+        }, 404);
     }
 }
