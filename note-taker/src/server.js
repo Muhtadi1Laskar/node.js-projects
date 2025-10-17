456c61dece1c3f1c4dc12d3a8894de8027875047
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-17 18:50:30 +0600

    Started the note-taker project

---
+++ note-taker/src/server.js
@@ -0,0 +1,13 @@
+import { createServer } from "node:http";
+
+const PORT = process.env.PORT || 8080;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "Hello World" }));
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
