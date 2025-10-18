749a783c8c149d719aa78317e4e9fc0f293dab98
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-18 15:00:06 +0600

    Added the getAllNotes controller

--- note-taker/src/controllers/note.controller.js
+++ note-taker/src/controllers/note.controller.js
@@ -1,10 +1,19 @@
-import { createNote } from "../services/note.service.js";
+import { createNote, getAllNote } from "../services/note.service.js";
 import { errorResponse, successResponse } from "../utils/response.js";
 
 export const makeNote = async (res, body) => {
     try {
         const note = await createNote(body);
         successResponse(res, note, 201);
+    } catch (error) {
+        errorResponse(res, { message: error.message }, 403);
+    }
+}
+
+export const getAllNotes = async (res, body) => {
+    try {
+        const notes = await getAllNote(body);
+        successResponse(res, notes, 201);
     } catch (error) {
         errorResponse(res, { message: error.message }, 403);
     }
