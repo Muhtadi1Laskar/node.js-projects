0ba052c8f90ed7b4310caa61a8d6de1fa1451233
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-19 22:21:45 +0600

    Added middleware to handle file upload

--- cryptography-service/src/middleware/fileHandler.js
+++ cryptography-service/src/middleware/fileHandler.js
@@ -6,7 +6,7 @@ const upload = multer({
     storage,
     limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
-        if(file.mimetype === "text/plain" || file.mimetype.startsWith("image/")) {
+        if (file.mimetype === "text/plain" || file.mimetype.startsWith("image/")) {
             cb(null, true);
         } else {
             cb(new Error("Invalid file type"), false);
