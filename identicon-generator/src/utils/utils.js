d7d0e78048d0ee2bb08ced976bd32b75f472a640
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-22 18:15:24 +0600

    Added function to parse the response body

--- identicon-generator/src/utils/utils.js
+++ identicon-generator/src/utils/utils.js
@@ -3,3 +3,18 @@ export const writeResponse = (res, data, statusCode = 200) => {
     res.end(JSON.stringify(data));
 }
 
+export const parseRequestBody = (req) => {
+    return new Promise((resolve, reject) => {
+        let body = '';
+
+        req.on("data", chunk => (body += chunk.toString()));
+        req.on("end", () => {
+            try {
+                resolve(JSON.parse(body));
+            } catch (error) {
+                resolve(body);
+            }
+        });
+        res.on("error", reject);
+    });
+}
