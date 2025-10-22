bfe71f11b2553580626917208fa86f721d997cfa
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-22 22:29:31 +0600

    Added schema for identicon generator|

---
+++ identicon-generator/src/routers/router.js
@@ -0,0 +1,8 @@
+import { identiconSchema } from "../schema/identicon.schema.js";
+
+const router = {
+    "POST:/generate-identicon": {
+        contorller: '',
+        schema: identiconSchema
+    }
+};
---
+++ identicon-generator/src/schema/identicon.schema.js
@@ -0,0 +1,4 @@
+export const identiconSchema = {
+    data: "string",
+    hash: "string"
+};
