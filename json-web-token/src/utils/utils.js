1c913a048a734794b256bcac1f2a03a3223ed230
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-29 03:16:34 +0600

    Added function to parse the response body

--- json-web-token/src/utils/utils.js
+++ json-web-token/src/utils/utils.js
@@ -3,5 +3,19 @@ export const writeResponse = (res, data, statusCode = 200) => {
     res.end(JSON.stringify(data));
 }
 
+export const parseRequest = (req) => {
+    return new Promise((resolve, reject) => {
+        let body = '';
 
+        req.on("data", chunk => (chunk += data));
+        req.on("end", () => {
+            try {
+                resolve(JSON.parse(body));
+            } catch (error) {
+                resolve(error);
+            }
+        });
+        req.on("error", reject);
+    });
+}
 
