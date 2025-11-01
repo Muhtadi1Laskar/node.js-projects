e3d17e6b12f95f4c7052331f52a867c673e4994c
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-11-02 03:54:15 +0600

    Added function to validate request body

---
+++ json-web-token/src/handlers/handler.js
@@ -0,0 +1,15 @@
+import { router } from "../routers/router.js";
+import { writeResponse } from "../utils/utils.js";
+
+export const handler = async (req, res) => {
+    const { url, method } = req;
+    const endpoint = `${method}:${url}`;
+    const route = router[endpoint];
+
+    if (!route) {
+        writeResponse(res, { message: "Invalid endpoint" }, 404);
+        return;
+    }
+
+    
+}
