0f003c8589ae9ffe910f6da4d04fa98e772f86a6
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-11-09 23:29:51 +0600

    Added the handler function

--- json-web-token/src/handlers/handler.js
+++ json-web-token/src/handlers/handler.js
@@ -1,14 +1,34 @@
 import { router } from "../routers/router.js";
-import { writeResponse } from "../utils/utils.js";
+import { parseRequest, validateSchema, writeResponse } from "../utils/utils.js";
 
 export const handler = async (req, res) => {
     const { url, method } = req;
     const endpoint = `${method}:${url}`;
     const route = router[endpoint];
 
-    if (!route) {
+    if (!route.controller) {
         writeResponse(res, { message: "Invalid endpoint" }, 404);
         return;
+    }
+
+    let body = {};
+    if (method === "POST") {
+        const rawBody = await parseRequest(req);
+        const { valid, message } = validateSchema(rawBody, route.scheme);
+
+        if (!valid) {
+            writeResponse(res, { message });
+            return;
+        }
+
+        body = rawBody;
+    }
+
+    try {
+        await route.controller(res, body);
+    } catch (error) {
+        console.error(error);
+        writeResponse(res, { message: error.message });
     }
 
 }
