964090ecd6549e23eb9c9357b0808c4a15cf4637
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-11-09 23:42:15 +0600

    Added controller for verifying JWT

--- json-web-token/src/controllers/jwt.controller.js
+++ json-web-token/src/controllers/jwt.controller.js
@@ -1,4 +1,4 @@
-import { createJWT } from "../services/jwt.services.js";
+import { createJWT, verifyJWT } from "../services/jwt.services.js";
 import { writeResponse } from "../utils/utils";
 
 export async function generateJWTController(res, req) {
@@ -11,6 +11,20 @@ export async function generateJWTController(res, req) {
         const token = createJWT(data, secretKey);
         writeResponse(res, { token });
     } catch (error) {
-        writeResponse(res, { error });
+        writeResponse(res, { message: error });
+    }
+}
+
+export async function verifyJWTController(res, req) {
+    const {
+        token,
+        secretKey
+    } = req;
+
+    try {
+        const isValidToken = verifyJWT(token, secretKey);
+        writeResponse(res, { isValidToken });
+    } catch (error) {
+        writeResponse(res, { message: error });
     }
 }
--- json-web-token/src/routers/router.js
+++ json-web-token/src/routers/router.js
@@ -1,12 +1,13 @@
 import { generateJWTController } from "../controllers/jwt.controller.js";
+import { generateJWTSchema, verifyJWTSchmea } from "../schema/jwt.schema.js";
 
 export const router = {
     "POST:/jwt/create": {
         contorller: generateJWTController,
-        schema: ''
+        schema: generateJWTSchema
     },
     "POST:/jwt/verify": {
         controller: '',
-        schema: ''
+        schema: verifyJWTSchmea
     }
 };
