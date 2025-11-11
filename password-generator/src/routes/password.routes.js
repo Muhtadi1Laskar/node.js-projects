80f28a2987dd4fc965d5632f0320c8c70393b6bb
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-11-12 04:58:03 +0600

    Added schema

--- password-generator/src/routes/password.routes.js
+++ password-generator/src/routes/password.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { validate } from "../middleware/validataionHandler.js";
+import {
+    passphraseSchema, passwordSchema, strengthSchema
+} from "../schema/password.schema.js";
 
 const router = express.Router();
 
-router.post("/generate", validate());
-router.post("/passphrase", validate());
-router.post("/strength", validate());
+router.post("/generate", validate(passwordSchema));
+router.post("/passphrase", validate(passphraseSchema));
+router.post("/strength", validate(strengthSchema));
 
 export default router;
