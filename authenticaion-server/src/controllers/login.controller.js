024f3bea28160345d7f7473559489573c117c099
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-12-02 01:33:18 +0600

    Added the login controller

--- authenticaion-server/src/controllers/login.controller.js
+++ authenticaion-server/src/controllers/login.controller.js
@@ -1,3 +1,11 @@
+import { login } from "../service/login.service.js";
+import { successResponse } from "../utils/response.js";
+
 export default async function loginController(req, res, next) {
-    
+    try {
+        const loginSuccess = await login(req.body);
+        successResponse(res, loginSuccess, 200);
+    } catch(error) {
+        next(error);
+    }
 }
