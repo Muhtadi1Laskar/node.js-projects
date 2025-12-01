46a3d2a7cc4d89259bfe51b4546b579de0e87193
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-12-02 01:20:24 +0600

    Updated the signup controller

--- authenticaion-server/src/controllers/signup.controller.js
+++ authenticaion-server/src/controllers/signup.controller.js
@@ -1,18 +1,10 @@
 import { signup } from "../service/signup.service.js";
-import { successResponse } from "../utils/response.js";
+import { errorResponse, successResponse } from "../utils/response.js";
 
 export default async function signupController(req, res, next) {
-    const {
-        email,
-        phone,
-        firstName,
-        lastName,
-        password
-    } = req.body;
-
     try {
         const user = await signup(req.body);
-        successResponse(res, user, 200);
+        successResponse(res, user, 201);
     } catch (error) {
         next(error);
     }
