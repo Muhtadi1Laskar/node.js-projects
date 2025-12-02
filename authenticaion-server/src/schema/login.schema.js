a99fecba06dda27e6e42c819ce52cac8c4512c7c
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-12-02 21:23:50 +0600

    Added the login service file

--- authenticaion-server/src/schema/login.schema.js
+++ authenticaion-server/src/schema/login.schema.js
@@ -11,14 +11,8 @@ export const loginSchema = Joi.object({
         }),
 
     password: Joi.string()
-        .min(8)
-        .max(128)
-        .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])/)
         .required()
         .messages({
-            'string.min': 'Password must be at least 8 characters',
-            'string.max': 'Password cannot exceed 128 characters',
-            'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
             'any.required': 'Password is required'
         }),
 });
--- authenticaion-server/src/service/login.service.js
+++ authenticaion-server/src/service/login.service.js
@@ -1,3 +1,30 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import singupModel from "../models/users.model.js";
+import { ApiError } from "../utils/error.js";
+
 export const login = async ({ email, password }) => {
-    
+    const savedUser = await singupModel.findOne({ email });
+
+    if (!savedUser || !(await bcrypt.compare(password, savedUser.password))) {
+        throw new ApiError(401, "Invalid email or password");
+    }
+
+    const payload = {
+        user: {
+            id: savedUser._id
+        }
+    };
+
+    const token = jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' },
+    );
+
+    return {
+        message: "Login successful",
+        token
+    };
 }
