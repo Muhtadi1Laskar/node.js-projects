b3c1840b49ceff42901454e85a19695438e34cf3
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-08-15 02:55:22 +0600

    Created schema for users

---
+++ web2-wallet/src/data/createUserTable.js
@@ -0,0 +1,26 @@
+import pool from "../config/db.js";
+
+const createrUserTable = async () => {
+    const queryText = `
+    CREATE EXTENTSION IF NOT EXISTS "pgcrypto";
+
+    CREATE TABLE IF NOT EXISTS users (
+        user_id UUID PRIMARY KEY gen_random_uuid(),
+        first_name VARCHAR(20) NOT NULL,
+        last_name VARCHAR(20) NOT NULL,
+        email VARCHAR(30) NOT NULL UNIQUE
+    );
+
+    CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
+    `;
+
+    try {
+        await pool.query(queryText);
+        console.log("User table created if not existed");
+    } catch (error) {
+        console.error("Failed to create user table: ", error.message);
+        throw error;
+    }
+}
+
+export default createrUserTable;
